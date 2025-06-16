7ebd950eeea0723eee916287b2f421839dbd5e4f
Gibson <gibsonlee08@gmail.com>
2025-06-16 16:33:04 -0500

    License, better formatting, readme all updated

--- PyScore.py
+++ PyScore.py
@@ -2,8 +2,15 @@ import dearpygui.dearpygui as dpg
 
 class GUIManager:
     def __init__(self):
-        self.width = 2000
-        self.height = 500
+        self.width = 1920
+        self.height = 1080
+        self.window_width = int(self.width / 2)
+        self.scoreboard_height = int(self.height / 4)
+        self.settings_height = self.scoreboard_height * 3
+        self.initial_t1_color = [0, 130, 0, 255]
+        self.initial_t2_color = [0, 130, 130, 255]
+        self.color_white = [255, 255, 255, 255]
+        self.alignment_width = 16
 
     @staticmethod
     def score_up(sender, app_data, user_data):
@@ -15,7 +22,10 @@ class GUIManager:
         :return: None
         """
         specifier = f"team{str(user_data)}_score"
-        current_score = int(dpg.get_value(specifier))
+        try:
+            current_score = int(dpg.get_value(specifier))
+        except ValueError:
+            current_score = 0
         current_score += 1
         dpg.set_value(specifier, value=current_score)
 
@@ -29,9 +39,28 @@ class GUIManager:
         :return: None
         """
         specifier = f"team{str(user_data)}_score"
-        current_score = int(dpg.get_value(specifier))
+        try:
+            current_score = int(dpg.get_value(specifier))
+        except ValueError:
+            current_score = 0
         current_score -= 1
         dpg.set_value(specifier, value=current_score)
+
+    @staticmethod
+    def set_score(sender, app_data, user_data):
+        """
+        Sets a team's score to a text input
+        :param sender:
+        :param app_data:
+        :param user_data:
+        :return: None
+        """
+        specifier = f"team{str(user_data)}_score"
+        try:
+            current_score = int(app_data)
+        except ValueError:
+            current_score = 0
+        dpg.set_value(specifier, current_score)
 
     @staticmethod
     def color_picker(sender, app_data, user_data):
@@ -59,59 +88,63 @@ class GUIManager:
         :param user_data:
         :return: None
         """
+        if app_data == "":
+            app_data = f"Team {user_data}"
         dpg.set_value(f"team{user_data}_name", f"{app_data}")
 
-    def define_fonts(self):
+    @staticmethod
+    def define_fonts():
         """
         Sets themes to use any font.
         :return: None
         """
         with dpg.font_registry():
             space_mono_regular = dpg.add_font("fonts/SpaceMono-Regular.ttf", 30)
-            space_mono_italic = dpg.add_font("fonts/SpaceMono-Italic.ttf", 30)
-            space_mono_bold = dpg.add_font("fonts/SpaceMono-Bold.ttf", 30)
-            space_mono_bolditalic = dpg.add_font("fonts/SpaceMono-BoldItalic.ttf", 30)
+            space_mono_scoreboard_scale = dpg.add_font("fonts/SpaceMono-Bold.ttf", size=100)
+            #space_mono_italic = dpg.add_font("fonts/SpaceMono-Italic.ttf", 30)
+            #space_mono_bold = dpg.add_font("fonts/SpaceMono-Bold.ttf", 30)
+            #space_mono_bolditalic = dpg.add_font("fonts/SpaceMono-BoldItalic.ttf", 30)
 
             dpg.bind_font(space_mono_regular)
-            dpg.bind_item_font(self.scoreboard, space_mono_bold)
+            dpg.bind_item_font("team1_scoreboard", space_mono_scoreboard_scale)
+            dpg.bind_item_font("team2_scoreboard", space_mono_scoreboard_scale)
 
-    @staticmethod
-    def define_themes():
+    def define_themes(self):
         """
         Creates and initially binds all themes.
         :return: None
         """
         with dpg.theme() as main_theme: # Theme as a base for all other formatting to go off of.
             with dpg.theme_component(dpg.mvAll):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, [255, 255, 255, 255], category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_Text, self.color_white, category=dpg.mvThemeCat_Core)
                 dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 8, category=dpg.mvAll)
 
         dpg.bind_theme(main_theme) # bind_theme() applies a theme to *everything*.
 
         with dpg.theme() as team_1_color: # This is used for team 1's accent color,
             with dpg.theme_component(dpg.mvAll):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, [255, 255, 255, 255], category=dpg.mvThemeCat_Core, tag="team1_color_theme")
+                dpg.add_theme_color(dpg.mvThemeCol_Text, self.color_white, category=dpg.mvThemeCat_Core, tag="team1_color_theme")
 
         dpg.bind_item_theme("team1_name", team_1_color) # bind_item_theme, as suggested, applies a theme to only
         dpg.bind_item_theme("team1_score", team_1_color) # a specific item.
 
         with dpg.theme() as team_2_color: # and this one is for team 2's accent.
             with dpg.theme_component(dpg.mvAll):
-                dpg.add_theme_color(dpg.mvThemeCol_Text, [255, 255, 255, 255], category=dpg.mvThemeCat_Core, tag="team2_color_theme")
+                dpg.add_theme_color(dpg.mvThemeCol_Text, self.color_white, category=dpg.mvThemeCat_Core, tag="team2_color_theme")
 
         dpg.bind_item_theme("team2_name", team_2_color)
         dpg.bind_item_theme("team2_score", team_2_color)
 
         with dpg.theme() as up_button_theme: # Green color for buttons that add a point
             with dpg.theme_component(dpg.mvAll):
-                dpg.add_theme_color(dpg.mvThemeCol_Button, value=[0, 130, 0, 255], category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_Button, value=[0, 100, 0, 255], category=dpg.mvThemeCat_Core)
 
         dpg.bind_item_theme("t1_score_up_button", up_button_theme)
         dpg.bind_item_theme("t2_score_up_button", up_button_theme)
 
         with dpg.theme() as down_button_theme: # and red for those that remove a point.
             with dpg.theme_component(dpg.mvAll):
-                dpg.add_theme_color(dpg.mvThemeCol_Button, value=[130, 0, 0, 255], category=dpg.mvThemeCat_Core)
+                dpg.add_theme_color(dpg.mvThemeCol_Button, value=[100, 0, 0, 255], category=dpg.mvThemeCat_Core)
 
         dpg.bind_item_theme("t1_score_down_button", down_button_theme)
         dpg.bind_item_theme("t2_score_down_button", down_button_theme)
@@ -122,31 +155,44 @@ class GUIManager:
         :return: None
         """
         # This first window is a parent to four items, all associated with Team 1's settings.
-        with dpg.window(label="Team 1 Settings", width=int(self.width / 5), height=self.height, pos=[0, 0],
+        with dpg.window(label="Team 1 Settings", width=self.window_width, height=self.settings_height, pos=[0, self.scoreboard_height],
                         no_move=True, no_resize=True, no_close=True, no_collapse=True) as self.t1_settings:
             dpg.add_button(label="Team 1 [+1]", tag="t1_score_up_button", callback=self.score_up, user_data=1)
             dpg.add_button(label="Team 1 [-1]", tag="t1_score_down_button", callback=self.score_down, user_data=1)
+            dpg.add_input_text(hint="Set score...", tag="t1_set_score", callback=self.set_score, user_data=1)
             dpg.add_input_text(hint="Rename...", tag="t1_rename", callback=self.rename_teams, user_data=1)
-            dpg.add_color_edit(tag="t1_color_picker", default_value=[255, 255, 255, 255], callback=self.color_picker, user_data=1)
-
-        # Nothing too fancy for this one. All the text has a color value, but it also is a part of a theme.
-        # Said theme can define all the formatting about a specific item, or you can define color here
-        # with the keyword "color="
-        with dpg.window(label="Scoreboard", width=int((self.width / 5) * 3), height=self.height, pos=[int(self.width / 5), 0],
-                        no_move=True, no_resize=True, no_close=True, no_collapse=True) as self.scoreboard:
-            dpg.add_text(default_value="Team 1", tag="team1_name")
-            dpg.add_text(default_value="0", tag="team1_score")
-            dpg.add_text(default_value="Team 2", tag="team2_name")
-            dpg.add_text(default_value="0", tag="team2_score")
+            dpg.add_color_edit(tag="t1_color_picker", default_value=self.color_white, callback=self.color_picker, user_data=1)
 
         # Note the "callback=" argument. This has the button call a function to do whatever you want in the
         # background along with some optional user_data.
-        with dpg.window(label="Team 2 Settings", width=int(self.width / 5), height=self.height, pos=[int((self.width / 5) * 4), 0],
+        with dpg.window(label="Team 2 Settings", width=self.window_width, height=self.settings_height, pos=[self.window_width, self.scoreboard_height],
                         no_move=True, no_resize=True, no_close=True, no_collapse=True) as self.t2_settings:
             dpg.add_button(label="Team 2 [+1]", tag="t2_score_up_button", callback=self.score_up, user_data=2)
             dpg.add_button(label="Team 2 [-1]", tag="t2_score_down_button", callback=self.score_down, user_data=2)
+            dpg.add_input_text(hint="Set score...", tag="t2_set_score", callback=self.set_score, user_data=2)
             dpg.add_input_text(hint="Rename...", tag="t2_rename", callback=self.rename_teams, user_data=2)
-            dpg.add_color_edit(tag="t2_color_picker", default_value=[255, 255, 255, 255], callback=self.color_picker, user_data=2)
+            dpg.add_color_edit(tag="t2_color_picker", default_value=self.color_white, callback=self.color_picker, user_data=2)
+
+        with dpg.window(label="Scoreboard", width=self.width, height=self.scoreboard_height, pos=[0, 0],
+                        no_move=True, no_resize=True, no_close=True, no_collapse=True, no_title_bar=True, no_scrollbar=True):
+            with dpg.group(horizontal=True, tag="team1_scoreboard") as self.team1_scores:
+                dpg.add_spacer(width=15)
+                dpg.add_text(default_value="0", tag="team1_score")
+                dpg.add_text(default_value=" - ")
+                dpg.add_text(default_value="Team 1", tag="team1_name")
+            with dpg.group(horizontal=True, tag="team2_scoreboard") as self.team2_scores:
+                dpg.add_spacer(width=15)
+                dpg.add_text(default_value="0", tag="team2_score")
+                dpg.add_text(default_value=" - ")
+                dpg.add_text(default_value="Team 2", tag="team2_name")
+
+    def keyboard_manager(self):
+        with dpg.handler_registry():
+            dpg.add_key_press_handler(key=dpg.mvKey_NumPad7, callback=self.score_up, user_data=1)
+            dpg.add_key_press_handler(key=dpg.mvKey_NumPad1, callback=self.score_down, user_data=1)
+
+            dpg.add_key_press_handler(key=dpg.mvKey_NumPad9, callback=self.score_up, user_data=2)
+            dpg.add_key_press_handler(key=dpg.mvKey_NumPad3, callback=self.score_down, user_data=2)
 
     def main_loop(self):
         dpg.create_context() # Start the main GUI process
@@ -154,6 +200,9 @@ class GUIManager:
         self.window_manager() # Actually create the windows, buttons, etc.
         self.define_fonts() # Load different fonts
         self.define_themes() # Use custom-made themes
+        self.keyboard_manager() # Start listening for keyboard inputs
+        self.color_picker(sender="", app_data=self.color_white, user_data=1) # Use initial color scheme already
+        self.color_picker(sender="", app_data=self.color_white, user_data=2) # loaded onto the color pickers
 
         dpg.create_viewport(title='PyScore - Scorekeeping', width=self.width, height=self.height)
         dpg.setup_dearpygui()
--- README.md
+++ README.md
@@ -3,3 +3,19 @@
 ![GitHub Commits](https://img.shields.io/github/commit-activity/t/gsl4295/PyScore)
 ![GitHub Issues](https://img.shields.io/github/issues/gsl4295/PyScore)<br>
 Simple python-powered scoreboard for use with my online webcasts.
+
+<img src="/images/GUI.png" width=500 /><br>
+
+### Features
+- User-inputted:
+  - Team names
+  - Team colors
+  - Score
+    - +1, -1, or by text input
+- Streaming-friendly output section
+  - BG hexadecimal (for color key): #252526
+
+### Setup
+- My setup uses the `space_mono` font family. If you want to replicate the image above,
+  - Download it [here](https://) on Google Fonts.
+  - Then, move the .ttf files over into a new `fonts/` directory at the root of this repo.
